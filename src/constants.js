a469362c9b7e2ae806b5fbb6b303efc2ebd135af
Jose Santiago Munar Ortiz <160024794+SantiagoMunarOrtiz@users.noreply.github.com>
2024-05-22 09:42:29 -0500

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,23 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I've made a couple of games in that language. I also like java and React. Anyway regardless of the language, I just like programming.
+    Here is my <https://github.com/SantiagoMunarOrtiz" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube and to learn . 
+  I also make game programming and i have to web page  . Go to see <a href="https://curriculumjosesantiagomunar.netlify.app/" target="_blank">My Projects</a>,
+  You'll learn how I built this portfolio you're currently playing through!`,
+  tv: `That's my TV. I've been watching my projects  :
+   <a href="https://curriculumjosesantiagomunar.netlify.app/" target="_blank">My Projects</a>, 
+  <a href="https://github.com/SantiagoMunarOrtiz" target="_blank">my git hub</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. 
+  Contact me at munarsantiago1@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js target="_blank">here</a>.`,
+  library: `There are a lot of programming books on my shelves. There is even one in english (I also speak english btw my level is b1).
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Santiago munar's portfolio, just close the tab.`,
+};
